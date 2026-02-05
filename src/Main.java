427060fdf0af4de6287e840487693808c2d03034
aylinates <aylinates591@gmail.com>
2026-02-06 02:33:30 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Automation system started successfully!");
+        try {
+            File file = new File("data.txt");
+            Scanner scanner = new Scanner(file);
+
+            int lineCount = 0;
+
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                lineCount++;
+            }
+
+            scanner.close();
+            System.out.println("Total lines in file: " + lineCount);
+
+        } catch (FileNotFoundException e) {
+            System. out.println("File not found!");
+        }
     }
 }
